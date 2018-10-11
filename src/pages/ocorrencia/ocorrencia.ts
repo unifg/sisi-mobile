281aec713cf0ff7bac153975e7caa4f8baf51da3
EdSilvaJr <ed.silva.jr89@gmail.com>
2018-10-10 23:38:07 -0300

    01.01 - feature/mob-0008 - Tela de Ocorrência Revisada

--- src/pages/ocorrencia/ocorrencia.ts
+++ src/pages/ocorrencia/ocorrencia.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD:src/pages/ocorrencia/ocorrencia.ts
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FeedPage } from '../feed/feed';
-=======
-import { AlertController, App, LoadingController, IonicPage, NavController, NavParams } from 'ionic-angular';
-import { RegisterPage } from '../register/register';
->>>>>>> 0aa5741d00c42c8475eab4b7bf2f0ca577718d29:src/pages/login/login.ts
 
 /**
  * Generated class for the OcorrenciaPage page.
@@ -19,62 +14,22 @@ import { RegisterPage } from '../register/register';
   selector: 'page-ocorrencia',
   templateUrl: 'ocorrencia.html',
 })
-<<<<<<< HEAD:src/pages/ocorrencia/ocorrencia.ts
 export class OcorrenciaPage {
-=======
-export class LoginPage {
 
-  public loginForm: any;
-  public backgroundImage = '../assets/img/background/globalbackground.jpg';
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
 
-  tabsPage = TabsPage;
->>>>>>> 0aa5741d00c42c8475eab4b7bf2f0ca577718d29:src/pages/login/login.ts
-
-  constructor(
-    public loadingCtrl: LoadingController,
-    public alertCtrl: AlertController,
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public app: App
-  ) { }
-
-  login() {
-    const loading = this.loadingCtrl.create({
-      duration: 500
-    });
-
-    loading.onDidDismiss(() => {
-      const alert = this.alertCtrl.create({
-        title: 'Logged in!',
-        subTitle: 'Thanks for logging in.',
-        buttons: ['Dismiss']
-      });
-      alert.present();
-    });
-
-    loading.present();
-
+    
   }
 
-  goToRegisterPage() {
-    this.navCtrl.push(RegisterPage);
-  }
-
-<<<<<<< HEAD:src/pages/ocorrencia/ocorrencia.ts
   ionViewDidLoad() {
     console.log('ionViewDidLoad OcorrenciaPage');
   }
 
-  public goToFeed() {
-    this.navCtrl.pop()
-=======
-  goToResetPassword() {
-    // this.navCtrl.push(ResetPasswordPage);
->>>>>>> 0aa5741d00c42c8475eab4b7bf2f0ca577718d29:src/pages/login/login.ts
+  public goToFeed(){
+    this.navCtrl.pop(FeedPage)
   }
 
-
-  public confirmar() {
+  public confirmar(){
     alert("Dados Enviados!")
   }
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
+
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
-<<<<<<< HEAD
 import { FeedPage } from '../feed/feed';
 import { OcorrenciaPage } from '../ocorrencia/ocorrencia';
-=======
-import {  RegisterPage } from '../register/register';
-import { RegisterPerfilPage } from '../registerPerfil/registerPerfil';
->>>>>>> 0aa5741d00c42c8475eab4b7bf2f0ca577718d29
 
 @Component({
   templateUrl: 'tabs.html'
@@ -18,15 +14,8 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
-<<<<<<< HEAD
   tab4Root = FeedPage;
   tab5Root = OcorrenciaPage;
-=======
-  tab4Root = RegisterPage;
-  tab5Root = RegisterPerfilPage
-
-  
->>>>>>> 0aa5741d00c42c8475eab4b7bf2f0ca577718d29
 
   constructor() {
 
