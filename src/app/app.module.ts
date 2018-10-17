28fd1f95bfdc2343f59c5ab2029320f683ae6895
Iago Neres <iagonascimento@mangue3.com>
2018-10-17 07:55:56 -0300

    01.01 - feature/mob-0003 - Resolvendo conflitos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,4 @@
 import { NgModule, ErrorHandler } from '@angular/core';
-<<<<<<< HEAD
-import { BrowserModule }          from '@angular/platform-browser';
-import { MyApp }                  from './app.component';
-import { AboutPage }    from '../pages/about/about';
-import { ContactPage }  from '../pages/contact/contact';
-import { HomePage }     from '../pages/home/home';
-import { TabsPage }     from '../pages/tabs/tabs';
-import { LoginPage }    from '../pages/login/login';
-
-
-import { StatusBar }    from '@ionic-native/status-bar';
-=======
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
@@ -22,7 +10,6 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { LoginPage }    from '../pages/login/login';
 import { StatusBar } from '@ionic-native/status-bar';
->>>>>>> d28e9d699e821ea93abdff14af322ec0d0fee1e9
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { RegisterPerfilPageModule } from '../pages/registerPerfil/registerPerfil.module';
 import { RegisterPageModule } from '../pages/register/register.module';
