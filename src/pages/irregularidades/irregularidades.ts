377cabcb7072526154fca24df99ed81fb4b4a19d
davi.holanda <davi.holanda@mv.com.br>
2018-10-19 11:17:33 -0300

    consertando erro pra tirar foto

--- src/pages/irregularidades/irregularidades.ts
+++ src/pages/irregularidades/irregularidades.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
-import { utils } from '../../util/util'
 
 
 /**
@@ -22,8 +21,18 @@ export class IrregularidadesPage {
   constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
   }
 
-  takePhoto() {
-    this.base64Image = utils.getPicture()
+  getPicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options).then((imageData) => {
+    this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Tratar erro.
+    })
   }
 
 
